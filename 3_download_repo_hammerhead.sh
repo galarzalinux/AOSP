283351d937e68e1372f0bcfc50bc8e9b3bf6cfaa
galarzalinux <galarzaingeniero@gmail.com>
2016-03-19 10:11:12 -0600

    Update 3_download_repo_hammerhead.sh

--- 3_download_repo_hammerhead.sh
+++ 3_download_repo_hammerhead.sh
@@ -6,6 +6,6 @@ echo "Enter full name and Google login user and password when prompt"
 echo "It \"WILL\" take a while"
 git config --global user.email "eltodopoderosogalarza@gmail.com"
 git config --global user.name "Jose Arturo Medina Galarza"
-repo init -u https://android.googlesource.com/platform/manifest -b android-6.0.1_r9
+repo init -u https://android.googlesource.com/platform/manifest -b android-6.0.1_r17
 echo "End of installation 3"
 echo "################################################################################################"
